3a6b7ee897a320450c707cc73d5906a270a77c88
Matheus Fidelis <msfidelis@outlook.com>
2016-11-20 19:33:42 -0200

    API de exemplo com Node e Mongo

--- NodeMongo/app/app.js
+++ NodeMongo/app/app.js
@@ -1,14 +1,67 @@
+//Exemplo de Web Service REST utilizando NodeJS e MongoDB em Containers Docker
+//
 var express = require('express');
+var mongo = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get("/", function (req, res) {
-	res.send("Teste");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Conexão com o MongoDB
+var mongoaddr = 'mongodb://' + process.env.MONGO_PORT_27017_TCP_ADDR + ':27017/testeapi';
+console.log(mongoaddr);
+mongo.connect(mongoaddr);
+
+//Esquema da collection do Mongo
+var taskListSchema = mongo.Schema({
+	descricao : { type: String },
+	concluido : Boolean,
+	updated_at: { type: Date, default: Date.now },
 });
 
-app.get("/api", function (req, res) {
-	res.json({"index1":"valor1", "index2":"valor2"});
+//Model da aplicação
+var Model = mongo.model('Tasks', taskListSchema);
+
+//POST - Adiciona um registro
+app.post("/api/add", function (req, res) {
+	var register = new Model({
+		'descricao' : req.body.descricao,
+		'concluido' : req.body.concluido
+	});
+	register.save(function (err) {
+		if (err) {
+			console.log(err);
+			res.send(err);
+			res.end();
+		}
+	});
+	res.send(register);
+	res.end();
 });
 
+//GET - Retorna todos os registros existentes no banco
+app.get("/api/all", function (req, res) {
+	Model.find(function(err, todos) {
+		if (err) {
+			res.json(err);
+		} else {
+			res.json(todos);
+		}
+	})
+});
+
+//DELETE - Deleta um registro
+app.delete("/api/delete/:id", function (req, res) {
+ Model.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+//Porta de escuta do servidor
 app.listen(8080, function() {
 	console.log('Funcionando');
 });
